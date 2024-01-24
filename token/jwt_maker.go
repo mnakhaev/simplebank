6dff25b43d110be87c4a294d33272e71f7a00c76
Magomed Nakhaev <magomed.nakhaev@acronis.com>
2024-01-24 15:36:14 +0300

    Added creation and verification of JWT and Paseto tokens

---
+++ token/jwt_maker.go
@@ -0,0 +1,56 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func (m *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(m.secretKey))
+}
+
+func (m *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC) // chosen algorithm is same
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(m.secretKey), nil
+	})
+	if err != nil {
+		var verr *jwt.ValidationError
+		if errors.As(err, &verr) && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+	return &JWTMaker{secretKey}, nil
+}
