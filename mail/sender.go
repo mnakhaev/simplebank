8081da1f8b7b5f7abcd471e94ce54533d950db97
Magomed Nakhaev <magomed.nakhaev@acronis.com>
2024-07-19 22:01:53 +0300

    Implemented sending of email

---
+++ mail/sender.go
@@ -0,0 +1,55 @@
+package mail
+
+import (
+	"fmt"
+	"net/smtp"
+
+	"github.com/jordan-wright/email"
+)
+
+const (
+	smtpAuthAddress   = "smtp.gmail.com"
+	smtpServerAddress = "smtp.gmail.com:587"
+)
+
+type EmailSender interface {
+	SendEmail(
+		subject, content string,
+		to, cc, bcc []string,
+		attachedFilesNames []string,
+	) error
+}
+
+type GmailSender struct {
+	name              string
+	fromEmailAddress  string
+	fromEmailPassword string
+}
+
+func NewGmailSender(name, fromEmailAddress, fromEmailPassword string) EmailSender {
+	return &GmailSender{
+		name:              name,
+		fromEmailAddress:  fromEmailAddress,
+		fromEmailPassword: fromEmailPassword,
+	}
+}
+
+func (sender *GmailSender) SendEmail(subject, content string, to, cc, bcc []string, attachedFilesNames []string) error {
+	e := email.NewEmail()
+	e.From = fmt.Sprintf("%s <%s>", sender.name, sender.fromEmailAddress)
+	e.Subject = subject
+	e.HTML = []byte(content)
+	e.To = to
+	e.Cc = cc
+	e.Bcc = bcc
+
+	for _, fileName := range attachedFilesNames {
+		_, err := e.AttachFile(fileName)
+		if err != nil {
+			return fmt.Errorf("failed to attach file %s: %w", fileName, err)
+		}
+	}
+
+	smtpAuth := smtp.PlainAuth("", sender.fromEmailAddress, sender.fromEmailPassword, smtpAuthAddress)
+	return e.Send(smtpServerAddress, smtpAuth)
+}
---
+++ mail/sender_test.go
@@ -0,0 +1,25 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/mnakhaev/simplebank/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	cfg, err := config.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(cfg.EmailSenderName, cfg.EmailSenderAddress, cfg.EmailSenderPassword)
+	subject := "Test email"
+	content := `
+	<h1>Hello</h1>
+	<p>This is a test email</p>
+`
+	to := []string{"sometestemail@corp.hahaha.com"}
+	attachFiles := []string{"../readme.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
