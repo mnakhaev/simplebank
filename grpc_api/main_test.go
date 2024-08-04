36a5bde06962c2a069a5b7839e9d77c8ffdf01b2
Magomed Nakhaev <magomed.nakhaev@acronis.com>
2024-08-04 19:38:33 +0300

    added test for gRPC API with authentication

--- grpc_api/main_test.go
+++ grpc_api/main_test.go
@@ -1,14 +1,18 @@
 package grpc_api
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/mnakhaev/simplebank/config"
 	db "github.com/mnakhaev/simplebank/db/sqlc"
+	"github.com/mnakhaev/simplebank/token"
 	"github.com/mnakhaev/simplebank/util"
 	"github.com/mnakhaev/simplebank/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, td worker.TaskDistributor) *Server {
@@ -20,4 +24,12 @@ func newTestServer(t *testing.T, store db.Store, td worker.TaskDistributor) *Ser
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationTypeBearer, accessToken)
+	md := metadata.MD{authorizationHeader: []string{bearerToken}}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
